d61ad466dfa9ec7ce3dbeb45123b3b36859d7753
patrick brisbin <pbrisbin@gmail.com>
2011-08-27 10:36:39 -0400

    install-all.sh: small rewrite

--- install-all.sh
+++ install-all.sh
@@ -1,17 +1,36 @@
-#!/bin/sh
+#!/bin/bash -e
 
-CABAL=cabal
+# allow a CABAL env var to override
+CABAL=${CABAL:-cabal}
 
 # install testing dependencies
-$CABAL install HUnit QuickCheck 'hspec >= 0.6.1 && < 0.7' || exit 1
+$CABAL install HUnit QuickCheck 'hspec >= 0.6.1 && < 0.7'
+
 # Note: Ignoring wai-handler-devel, wai-handler-webkit
-PACKAGES="wai wai-test wai-extra warp wai-app-static wai-handler-fastcgi wai-handler-launch wai-handler-scgi warp-static"
-for package in $PACKAGES
-do
-    echo Installing $package
-    cd $package
-    ($CABAL configure --enable-tests ||
-        ($CABAL install --only-dependencies && $CABAL configure --enable-tests)
-    ) && $CABAL build && $CABAL test && ./Setup.lhs install || exit 1
-    cd ..
+pkgs=( wai
+       wai-test
+       wai-extra
+       warp
+       wai-app-static
+       wai-handler-fastcgi
+       wai-handler-launch
+       wai-handler-scgi
+       warp-static
+     )
+
+for pkg in "${pkgs[@]}"; do
+  echo "Installing $pkg..."
+
+  (
+    cd "./$pkg"
+
+    if ! $CABAL configure --enable-tests; then
+      $CABAL install --only-dependencies
+      $CABAL configure --enable-tests
+    fi
+
+    $CABAL build
+    $CABAL test
+    ./Setup.lhs install
+  )
 done
