a88764b9b2e5f59aad5f9ca3cfa3299a7b00eff1
Michael Snoyman <michael@snoyman.com>
2011-12-14 09:22:32 +0200

    package-list.sh

---
+++ package-list.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pkgs=(        ./wai
+                  ./wai-test
+                  ./wai-extra
+                  ./warp
+                  ./wai-app-static
+                  ./wai-handler-fastcgi
+                  ./wai-handler-launch
+                  ./wai-handler-scgi
+                  ./warp-static
+                  ./wai-websockets        )
