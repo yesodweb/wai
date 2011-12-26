0e7edb5fb1c5bb3767dbd8f9594eaed941b75015
Greg Weber <greg@gregweber.info>
2011-12-25 23:34:47 -0300

    add logStdoutDev and logHandleDev
    These use ByteString instead of Lazy Text

--- wai-extra/Network/Wai/Middleware/RequestLogger.hs
+++ wai-extra/Network/Wai/Middleware/RequestLogger.hs
@@ -2,28 +2,35 @@
 module Network.Wai.Middleware.RequestLogger
     ( logStdout
     , logHandle
+    , logStdoutDev
+    , logHandleDev
     , logStdoutDevLT
     , logHandleDevLT
     ) where
 
 import System.IO (stdout)
 import qualified Data.ByteString as BS
+import Data.ByteString.Char8 (pack)
 import Control.Monad.IO.Class (liftIO)
 import Network.Wai (Request(..), Middleware)
 import System.Log.FastLogger
 import Network.HTTP.Types as H
 import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Encoding as TE
 
 import Network.Wai.Parse (parseRequestBody, lbsSink, fileName, Param, File)
 import qualified Data.ByteString.Lazy as LBS
 import Data.Enumerator (run_, ($$), enumList)
 import Data.Enumerator.List (consume)
 import System.IO (hPutStrLn, stderr)
-import Data.ByteString.Char8 (unpack)
 
--- | lik @logHandle@, but prints to 'stdout'
+-- | like @logHandle@, but prints to 'stdout'
 logStdout :: Middleware
 logStdout = logHandle $ \bs -> hPutLogStr stdout [LB bs]
+
+-- | like @logHandleDev@, but prints to 'stdout'
+logStdoutDev :: Middleware
+logStdoutDev = logHandleDev $ \bs -> hPutLogStr stdout [LB bs]
 
 -- | Prints a message using the given callback function for each request.
 -- Designed for fast production use at the expense of convenience.
@@ -50,12 +57,17 @@ toBS = id
 logStdoutDevLT :: Middleware
 logStdoutDevLT = logHandleDevLT $ hPutStrLn stderr . LT.unpack
 
+-- | logHandleDev, but expects Lazy Text instead of a ByteString
+logHandleDevLT :: (LT.Text -> IO ()) -> Middleware
+logHandleDevLT cb app req =
+    logHandleDev (\msg -> cb $ LT.fromStrict $ TE.decodeUtf8 msg) app req
+
 -- | Prints a message using the given callback function for each request.
 -- This is not for serious production use- it is inefficient.
 -- It immediately consumes a POST body and fills it back in and is otherwise inefficient
 -- For production use use module Network.Wai.Middleware.RequestLogger
-logHandleDevLT :: (LT.Text -> IO ()) -> Middleware
-logHandleDevLT cb app req = do
+logHandleDev :: (BS.ByteString -> IO ()) -> Middleware
+logHandleDev cb app req = do
     body <- consume
     postParams <- if any (requestMethod req ==) ["GET", "HEAD"]
       then return []
@@ -63,19 +75,22 @@ logHandleDevLT cb app req = do
               return $ collectPostParams postParams
     let getParams = map emptyGetParam $ queryString req
 
-    liftIO $ cb $ LT.pack $ concat
-        [ unpack $ requestMethod req
+    liftIO $ cb $ BS.concat
+        [ requestMethod req
         , " "
-        , unpack $ rawPathInfo req
+        , rawPathInfo req
         , "\n"
-        , (++) "Accept: " $ maybe "" unpack $ lookup "Accept" $ requestHeaders req
-        , paramsToStr  "GET " getParams
-        , paramsToStr "POST " postParams
+        , "Accept: "
+        , maybe "" id $ lookup "Accept" $ requestHeaders req
+        , paramsToBS  "GET " getParams
+        , paramsToBS "POST " postParams
         ]
     -- we just consumed the body- fill the enumerator back up so it is available again
     liftIO $ run_ $ enumList 1 body $$ app req
   where
-    paramsToStr prefix params = if null params then "" else "\n" ++ prefix ++ (show params)
+    paramsToBS prefix params =
+      if null params then ""
+        else BS.concat ["\n", prefix, pack (show params)]
 
     allPostParams req' body = run_ $ enumList 1 body $$ parseRequestBody lbsSink req'
 
