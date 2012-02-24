b7fc5ee58d48b0d65063cc94b53226c75e64855b
Greg Weber <greg@gregweber.info>
2012-02-24 10:01:02 -0800

    add rewritePure, remove autoHtmlRewrite

--- wai-extra/Network/Wai/Middleware/Rewrite.hs
+++ wai-extra/Network/Wai/Middleware/Rewrite.hs
@@ -1,39 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.Wai.Middleware.Rewrite
-    ( rewrite, autoHtmlRewrite
+    ( rewrite, rewritePure
     ) where
 
 import Network.Wai
-import System.Directory (doesFileExist)
 import Control.Monad.IO.Class (liftIO)
-import Data.Text (Text, unpack, pack)
+import Data.Text (Text)
 import qualified Data.Text.Encoding as TE
 import qualified Data.Text as T
 import Network.HTTP.Types as H
 
 
 -- | rewrite based on your own conversion rules
--- Example usage: rewrite (autoHtmlRewrite "static")
 rewrite :: ([Text] -> H.RequestHeaders -> IO [Text]) -> Middleware
 rewrite convert app req = do
   newPathInfo <- liftIO $ convert (pathInfo req) (requestHeaders req)
   let rawPInfo = TE.encodeUtf8 $ T.intercalate "/" newPathInfo
   app req { pathInfo = newPathInfo, rawPathInfo =  rawPInfo }
 
--- | example rewriter
--- We don't recommend normally checking the file system on every request - this is just an example.
--- Implements 2 rules for static html re-writes
---   1) for a directory foo/, check for foo/index.html
---   2) for a non-directory bar, check for bar.html
--- Do the rewrite only if the html file exists.
-autoHtmlRewrite :: String -> [Text] -> H.RequestHeaders -> IO [Text]
-autoHtmlRewrite staticDir pieces' _ = do
-    fe <- doesFileExist $ staticDir ++ "/" ++ reWritePath
-    return $ if fe then map pack reWritePieces else pieces'
-  where
-    pieces = map unpack pieces'
-    reWritePath = concat $ map ((:) '/') reWritePieces
-    reWritePieces =
-       if (null pieces) || (null $ last pieces)
-          then pieces ++  ["index.html"]
-          else (init pieces) ++ [(last pieces) ++ ".html"]
+-- | rewrite based on your own conversion rules
+-- Example convert function:
+
+-- staticConvert :: [Text] -> H.RequestHeaders -> [Text]
+-- staticConvert pieces _ = piecesConvert pieces
+--   where
+--    piecesConvert [] = ["static", "html", "pages.html"]
+--    piecesConvert route@("pages":_) = "static":"html":route
+rewritePure :: ([Text] -> H.RequestHeaders -> [Text]) -> Middleware
+rewritePure convert app req =
+  let pInfo = convert (pathInfo req) (requestHeaders req)
+      rawPInfo = TE.encodeUtf8 $ T.intercalate "/" pInfo
+  in  app req { pathInfo = pInfo, rawPathInfo =  rawPInfo }
