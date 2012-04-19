341f94551a63f5a30e5c2cb30531e9c3f01418e1
Lyndon Maydwell <maydwell@gmail.com>
2012-04-19 17:03:21 +0800

    Adding documentation

--- wai-extra/Network/Wai/Middleware/MethodOverridePost.hs
+++ wai-extra/Network/Wai/Middleware/MethodOverridePost.hs
@@ -1,14 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Network.Wai.Middleware.MethodOverridePost ( methodOverridePost) where
+-----------------------------------------------------------------
+-- |
+-- Module : Network.Wai.Middleware.MethodOverridePost
+--
+-- Changing of request-method via first post-parameter "_method".
+--
+-----------------------------------------------------------------
+module Network.Wai.Middleware.MethodOverridePost
+  ( methodOverridePost
+  ) where
 
 import Network.Wai
+import Network.HTTP.Types           (parseQuery)
 import Data.Monoid                  (mconcat)
 import Data.Conduit.Lazy            (lazyConsume)
-import Network.HTTP.Types           (parseQuery)
 import Control.Monad.Trans.Resource (ResourceT)
 import Data.Conduit.List            (sourceList)
 
+-- | Override the request-method by setting the first POST key/value
+-- to "_method" / <request-method>.
+--
+-- * Only accepts "application/x-www-form-urlencoded" POSTs.
 methodOverridePost :: Middleware
 methodOverridePost app req = case lookup "Content-Type" (requestHeaders req) of
   Just "application/x-www-form-urlencoded" -> setPost req >>= app
