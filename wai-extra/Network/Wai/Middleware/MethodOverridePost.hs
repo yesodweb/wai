288b8fbe511fccfac11a572b6a2ffc03db8d42c1
Lyndon Maydwell <maydwell@gmail.com>
2012-04-19 12:41:20 +0800

    Adding a POST variant of methodOverride

---
+++ wai-extra/Network/Wai/Middleware/MethodOverridePost.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Network.Wai.Middleware.MethodOverridePost ( methodOverridePost) where
+
+import Network.Wai
+import Data.Monoid                  (mconcat)
+import Data.Conduit.Lazy            (lazyConsume)
+import Network.HTTP.Types           (parseQuery)
+import Control.Monad.Trans.Resource (ResourceT)
+
+methodOverridePost :: Middleware
+methodOverridePost app req = do
+  case lookup "Content-Type" (requestHeaders req) of Just "application/x-www-form-urlencoded" -> setPost req >>= app
+                                                     _                                        -> app req
+
+setPost :: Request -> ResourceT IO Request
+setPost req = do
+  body <- lazyConsume (requestBody req)
+  case parseQuery $ mconcat body of (("method_", Just newmethod):_) -> return $ req {requestMethod = newmethod}
+                                    _                               -> return req
