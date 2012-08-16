9b525de04a14b98badc85fc66e945d50c1eb73a9
Kazu Yamamoto <kazu@iij.ad.jp>
2012-08-16 15:57:37 +0900

    adding test for composeHeader.

---
+++ warp/test/ResponseHeaderSpec.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module ResponseHeaderSpec where
+
+import Data.ByteString
+import qualified Network.HTTP.Types as H
+import Network.Wai.Handler.Warp.ResponseHeader
+import Test.Hspec
+
+spec :: Spec
+spec = describe "composeHeader" $ do
+    it "composes a HTTP header" $
+        composeHeader H.http11 H.ok200 headers `shouldBe` composedHeader
+
+headers :: H.ResponseHeaders
+headers = [
+    ("Date", "Mon, 13 Aug 2012 04:22:55 GMT")
+  , ("Content-Lenght", "151")
+  , ("Server", "Mighttpd/2.5.8")
+  , ("Last-Modified", "Fri, 22 Jun 2012 01:18:08 GMT")
+  , ("Content-Type", "text/html")
+  ]
+
+composedHeader :: ByteString
+composedHeader = "HTTP/1.1 200 OK\r\nDate: Mon, 13 Aug 2012 04:22:55 GMT\r\nContent-Lenght: 151\r\nServer: Mighttpd/2.5.8\r\nLast-Modified: Fri, 22 Jun 2012 01:18:08 GMT\r\nContent-Type: text/html\r\n\r\n"
