2b33ba1846401cc19b480d5762b7c633cf38c9a7
Kazu Yamamoto <kazu@iij.ad.jp>
2013-10-30 15:44:06 +0900

    adding indexResponseHeader.

--- warp/Network/Wai/Handler/Warp/Header.hs
+++ warp/Network/Wai/Handler/Warp/Header.hs
@@ -15,7 +15,7 @@ type IndexedHeader = Array Int (Maybe HeaderValue)
 ----------------------------------------------------------------
 
 indexRequestHeader :: RequestHeaders -> IndexedHeader
-indexRequestHeader hdr = traverseHeader hdr requestMaxIndex
+indexRequestHeader hdr = traverseHeader hdr requestMaxIndex requestKeyIndex
 
 idxContentLength,idxTransferEncoding,idxExpect :: Int
 idxConnection,idxRange,idxHost :: Int
@@ -30,6 +30,15 @@ idxHost             = 5
 requestMaxIndex :: Int
 requestMaxIndex     = 5
 
+requestKeyIndex :: HeaderName -> Int
+requestKeyIndex "content-length"    = idxContentLength
+requestKeyIndex "transfer-encoding" = idxTransferEncoding
+requestKeyIndex "expect"            = idxExpect
+requestKeyIndex "connection"        = idxConnection
+requestKeyIndex "range"             = idxRange
+requestKeyIndex "host"              = idxHost
+requestKeyIndex _                   = -1
+
 -- | Default 'IndexedHeader' for HTTP Request.
 --   All valuers are 'Nothing' by default.
 --   They correspond to \"Content-Length\", \"Transfer-Encoding\",
@@ -39,8 +48,28 @@ defaultIndexRequestHeader = array (0,requestMaxIndex) [(i,Nothing)|i<-[0..reques
 
 ----------------------------------------------------------------
 
-traverseHeader :: [Header] -> Int -> IndexedHeader
-traverseHeader hdr maxidx = runSTArray $ do
+indexResponseHeader :: ResponseHeaders -> IndexedHeader
+indexResponseHeader hdr = traverseHeader hdr responseMaxIndex responseKeyIndex
+
+idxContentRange,idxServer :: Int
+--idxContentLength = 0
+idxContentRange  = 1
+idxServer        = 2
+
+-- | The size for 'IndexedHeader' for HTTP Response.
+responseMaxIndex :: Int
+responseMaxIndex = 2
+
+responseKeyIndex :: HeaderName -> Int
+responseKeyIndex "content-length" = idxContentLength
+responseKeyIndex "content-range"  = idxContentRange
+responseKeyIndex "server"         = idxServer
+responseKeyIndex _                = -1
+
+----------------------------------------------------------------
+
+traverseHeader :: [Header] -> Int -> (HeaderName -> Int) -> IndexedHeader
+traverseHeader hdr maxidx getIndex = runSTArray $ do
     arr <- newArray (0,maxidx) Nothing
     mapM_ (insert arr) hdr
     return arr
@@ -49,13 +78,4 @@ traverseHeader hdr maxidx = runSTArray $ do
       | idx == -1 = return ()
       | otherwise = writeArray arr idx (Just val)
       where
-        idx = keyIndex key
-
-keyIndex :: HeaderName -> Int
-keyIndex "content-length"    = idxContentLength
-keyIndex "transfer-encoding" = idxTransferEncoding
-keyIndex "expect"            = idxExpect
-keyIndex "connection"        = idxConnection
-keyIndex "range"             = idxRange
-keyIndex "host"              = idxHost
-keyIndex _                   = -1
+        idx = getIndex key
