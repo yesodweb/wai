84dae37c2ba829e0d4d28ee8419e5e7e48840939
Michael Snoyman <michael@snoyman.com>
2014-03-09 07:47:19 +0200

    Drop fast-logger-2.0 support in wai-extra

--- wai-extra/Network/Wai/Middleware/RequestLogger/Internal.hs
+++ wai-extra/Network/Wai/Middleware/RequestLogger/Internal.hs
@@ -4,9 +4,6 @@
 -- https://github.com/yesodweb/wai/issues/192
 module Network.Wai.Middleware.RequestLogger.Internal
     ( module Network.Wai.Middleware.RequestLogger.Internal
-#if !MIN_VERSION_fast_logger(2, 1, 0)
-    , logToByteString
-#endif
     ) where
 
 import Data.ByteString (ByteString)
@@ -14,31 +11,10 @@ import Network.Wai.Logger (clockDateCacher)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Monad (forever)
 
-#if MIN_VERSION_fast_logger(2, 1, 0)
 import System.Log.FastLogger (LogStr, fromLogStr)
 
 logToByteString :: LogStr -> ByteString
 logToByteString = fromLogStr
-
-#else
-
-import System.Log.FastLogger (LogStr, logStrBuilder)
-
-#if MIN_VERSION_bytestring(0, 10, 2)
-import Data.ByteString.Builder (toLazyByteString, Builder)
-import qualified Data.ByteString.Lazy as L
-
-toByteString :: Builder -> ByteString
-toByteString = L.toStrict . toLazyByteString
-
-#else
-import Blaze.ByteString.Builder (toByteString)
-#endif
-
-logToByteString :: LogStr -> ByteString
-logToByteString = toByteString . logStrBuilder
-
-#endif
 
 getDateGetter :: IO () -- ^ flusher
               -> IO (IO ByteString)
