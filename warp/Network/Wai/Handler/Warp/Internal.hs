9a66a226c18f2db94a4bb85a9aa5d1c3e5e2ece6
Michael Snoyman <michael@snoyman.com>
2014-03-19 16:22:13 +0200

    Provide settings getters from .Internal

--- warp/Network/Wai/Handler/Warp/Internal.hs
+++ warp/Network/Wai/Handler/Warp/Internal.hs
@@ -1,5 +1,21 @@
+{-# OPTIONS_GHC -fno-warn-deprecations #-}
 module Network.Wai.Handler.Warp.Internal
     ( Settings (..)
+    , getOnOpen
+    , getOnClose
+    , getOnException
     ) where
 
 import Network.Wai.Handler.Warp.Settings (Settings (..))
+import Network.Socket (SockAddr)
+import Network.Wai (Request)
+import Control.Exception (SomeException)
+
+getOnOpen :: Settings -> SockAddr -> IO Bool
+getOnOpen = settingsOnOpen
+
+getOnClose :: Settings -> SockAddr -> IO ()
+getOnClose = settingsOnClose
+
+getOnException :: Settings -> Maybe Request -> SomeException -> IO ()
+getOnException = settingsOnException
