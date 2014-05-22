e74d6949defbda201cc1d18a6a7cb72067cb8e14
Michael Snoyman <michael@snoyman.com>
2014-05-22 20:39:38 +0300

    Fix eventsource

--- wai-extra/Network/Wai/EventSource.hs
+++ wai-extra/Network/Wai/EventSource.hs
@@ -5,63 +5,34 @@
 module Network.Wai.EventSource (
     ServerEvent(..),
     eventSourceAppChan,
-    eventSourceAppSource,
-    eventSourceAppIO,
-    sourceToSource
+    eventSourceAppIO
     ) where
 
 import           Blaze.ByteString.Builder (Builder)
+import           Data.Function (fix)
 import           Control.Concurrent.Chan (Chan, dupChan, readChan)
 import           Control.Monad.IO.Class (liftIO)
-import           Data.Conduit
-import qualified Data.Conduit.List as CL
 import           Network.HTTP.Types (status200)
-import           Network.Wai (Application, Response, responseSource)
+import           Network.Wai (Application, Response, responseStream)
 
 import Network.Wai.EventSource.EventStream
 
 -- | Make a new WAI EventSource application reading events from
 -- the given channel.
 eventSourceAppChan :: Chan ServerEvent -> Application
-eventSourceAppChan chan _ = do
-  chan' <- liftIO $ dupChan chan
-  return $ response chanToSource chan'
-
--- | Make a new WAI EventSource application reading events from
--- the given source.
-eventSourceAppSource :: Source IO ServerEvent -> Application
-eventSourceAppSource src _ = return $ response sourceToSource src
+eventSourceAppChan chan req sendResponse = do
+    chan' <- liftIO $ dupChan chan
+    eventSourceAppIO (readChan chan') req sendResponse
 
 -- | Make a new WAI EventSource application reading events from
 -- the given IO action.
 eventSourceAppIO :: IO ServerEvent -> Application
-eventSourceAppIO act _ = return $ response ioToSource act
-
-response :: (a -> Source IO (Flush Builder)) -> a -> Response
-response f a = responseSource status200 [("Content-Type", "text/event-stream")] $ f a
-
-chanToSource :: Chan ServerEvent -> Source IO (Flush Builder)
-chanToSource = ioToSource . readChan
-
-ioToSource :: IO ServerEvent -> Source IO (Flush Builder)
-ioToSource act =
-    loop
-  where
-    loop = do
-        x <- liftIO act
-        case eventToBuilder x of
-            Nothing -> return ()
-            Just y -> do
-                yield $ Chunk y
-                yield Flush
-                loop
-
--- | Convert a ServerEvent source into a Builder source of serialized
--- events.
-sourceToSource :: Monad m => Source m ServerEvent -> Source m (Flush Builder)
-sourceToSource src = src $= CL.concatMap eventToFlushBuilder
-  where
-    eventToFlushBuilder event =
-        case eventToBuilder event of
-            Nothing -> []
-            Just x -> [Chunk x, Flush]
+eventSourceAppIO src _ sendResponse =
+    sendResponse $ responseStream
+        status200
+        [("Content-Type", "text/event-stream")]
+        $ \sendChunk flush -> fix $ \loop -> do
+            se <- src
+            case eventToBuilder se of
+                Nothing -> return ()
+                Just b  -> sendChunk b >> flush >> loop
