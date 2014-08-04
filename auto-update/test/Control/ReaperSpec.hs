875089646b81c89dfdfebc9467b1b807a6d84e83
Michael Snoyman <michael@snoyman.com>
2014-08-04 10:05:37 +0300

    Control.Reaper

---
+++ auto-update/test/Control/ReaperSpec.hs
@@ -0,0 +1,33 @@
+module Control.ReaperSpec (spec) where
+
+import Control.Reaper
+import Control.Concurrent
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Data.IORef
+
+type Item = (Int, IORef Int)
+
+spec :: Spec
+spec = prop "works" $ \is -> do
+    addItem <- reaper defaultReaperSettings
+        { reaperAction = mkListAction $ \(i, ref) -> do
+            modifyIORef ref succ
+            return $ if i > 1
+                then Just (pred i, ref)
+                else Nothing
+        , reaperDelay = 1000
+        }
+
+    let mkTest i = do
+            ref <- newIORef 0
+            let expected = (abs i `mod` 10) + 1
+            addItem (expected, ref)
+            return (expected, ref)
+    tests <- mapM mkTest is
+
+    let test (expected, ref) = do
+            actual <- readIORef ref
+            actual `shouldBe` (expected :: Int)
+    threadDelay 100000
+    mapM_ test tests
