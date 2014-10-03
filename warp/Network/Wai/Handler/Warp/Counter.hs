f65706c7065fb13a6a18b9c00497c99db3447ea2
Michael Snoyman <michael@snoyman.com>
2014-10-03 08:28:37 +0300

    Revert "Use atomic-primops for Counter"
    This reverts commit 0247207811dcc66374199efe5da7ca04a2c34d50.

--- warp/Network/Wai/Handler/Warp/Counter.hs
+++ warp/Network/Wai/Handler/Warp/Counter.hs
@@ -6,19 +6,19 @@ module Network.Wai.Handler.Warp.Counter (
   , decrease
   ) where
 
-import qualified Data.Atomics.Counter.Unboxed as C
+import Network.Wai.Handler.Warp.IORef
 import Control.Applicative ((<$>))
 
-newtype Counter = Counter C.AtomicCounter
+newtype Counter = Counter (IORef Int)
 
 newCounter :: IO Counter
-newCounter = Counter <$> C.newCounter 0
+newCounter = Counter <$> newIORef 0
 
 isZero :: Counter -> IO Bool
-isZero (Counter ref) = (== 0) <$> C.readCounter ref
+isZero (Counter ref) = (== 0) <$> readIORef ref
 
 increase :: Counter -> IO ()
-increase (Counter ref) = C.incrCounter_ 1 ref
+increase (Counter ref) = atomicModifyIORef' ref $ \x -> (x + 1, ())
 
 decrease :: Counter -> IO ()
-decrease (Counter ref) = C.incrCounter_ (-1) ref
+decrease (Counter ref) = atomicModifyIORef' ref $ \x -> (x - 1, ())
