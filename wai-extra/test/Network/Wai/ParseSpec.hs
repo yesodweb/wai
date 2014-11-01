79e2d58ab0c600cee6cd42350d853d366d9262cc
Simon Hengel <sol@typeful.net>
2014-11-02 00:06:50 +0800

    wai-extra: Split caseParseRequestBody into separate spec items
    (one assertion per test)

--- wai-extra/test/Network/Wai/ParseSpec.hs
+++ wai-extra/test/Network/Wai/ParseSpec.hs
@@ -32,7 +32,8 @@ spec = do
             , ("text/plain; charset=UTF-8 ; boundary = foo", "text/plain", [("charset", "UTF-8"), ("boundary", "foo")])
             ]
     it "parseHttpAccept" caseParseHttpAccept
-    it "parseRequestBody" caseParseRequestBody
+    describe "parseRequestBody" $ do
+      caseParseRequestBody
     it "multipart with plus" caseMultipartPlus
     it "multipart with multiple attributes" caseMultipartAttrs
     it "urlencoded with plus" caseUrlEncPlus
@@ -60,67 +61,68 @@ parseRequestBody' sink (SRequest req bod) =
                             x:y -> (y, x)
             sinkRequestBody sink rbt rb
 
-caseParseRequestBody :: Assertion
-caseParseRequestBody =
-    t
+caseParseRequestBody :: Spec
+caseParseRequestBody = do
+  it "parsing post x-www-form-urlencoded" $ do
+    let content1 = "foo=bar&baz=bin"
+    let ctype1 = "application/x-www-form-urlencoded"
+    result1 <- parseRequestBody' lbsBackEnd $ toRequest ctype1 content1
+    result1 `shouldBe` ([("foo", "bar"), ("baz", "bin")], [])
+
+  let ctype2 = "multipart/form-data; boundary=AaB03x"
+  let expectedsmap2 =
+          [ ("title", "A File")
+          , ("summary", "This is my file\nfile test")
+          ]
+  let textPlain = "text/plain; charset=iso-8859-1"
+  let expectedfile2 =
+          [("document", FileInfo "b.txt" textPlain "This is a file.\nIt has two lines.")]
+  let expected2 = (expectedsmap2, expectedfile2)
+
+  it "parsing post multipart/form-data" $ do
+    result2 <- parseRequestBody' lbsBackEnd $ toRequest ctype2 content2
+    result2 `shouldBe` expected2
+
+  it "parsing post multipart/form-data 2" $ do
+    result2' <- parseRequestBody' lbsBackEnd $ toRequest' ctype2 content2
+    result2' `shouldBe` expected2
+
+
+  let ctype3 = "multipart/form-data; boundary=----WebKitFormBoundaryB1pWXPZ6lNr8RiLh"
+  let expectedsmap3 = []
+  let expectedfile3 = [("yaml", FileInfo "README" "application/octet-stream" "Photo blog using Hack.\n")]
+  let expected3 = (expectedsmap3, expectedfile3)
+
+  it "parsing actual post multipart/form-data" $ do
+    result3 <- parseRequestBody' lbsBackEnd $ toRequest ctype3 content3
+    result3 `shouldBe` expected3
+
+  it "parsing actual post multipart/form-data 2" $ do
+    result3' <- parseRequestBody' lbsBackEnd $ toRequest' ctype3 content3
+    result3' `shouldBe` expected3
   where
     content2 =
-        "--AaB03x\n" <>
-        "Content-Disposition: form-data; name=\"document\"; filename=\"b.txt\"\n" <>
-        "Content-Type: text/plain; charset=iso-8859-1\n\n" <>
-        "This is a file.\n" <>
-        "It has two lines.\n" <>
-        "--AaB03x\n" <>
-        "Content-Disposition: form-data; name=\"title\"\n" <>
-        "Content-Type: text/plain; charset=iso-8859-1\n\n" <>
-        "A File\n" <>
-        "--AaB03x\n" <>
-        "Content-Disposition: form-data; name=\"summary\"\n" <>
-        "Content-Type: text/plain; charset=iso-8859-1\n\n" <>
-        "This is my file\n" <>
-        "file test\n" <>
-        "--AaB03x--"
-    content3 = "------WebKitFormBoundaryB1pWXPZ6lNr8RiLh\r\nContent-Disposition: form-data; name=\"yaml\"; filename=\"README\"\r\nContent-Type: application/octet-stream\r\n\r\nPhoto blog using Hack.\n\r\n------WebKitFormBoundaryB1pWXPZ6lNr8RiLh--\r\n"
-    t = do
-        let content1 = "foo=bar&baz=bin"
-        let ctype1 = "application/x-www-form-urlencoded"
-        result1 <- parseRequestBody' lbsBackEnd $ toRequest ctype1 content1
-        assertEqual "parsing post x-www-form-urlencoded"
-                    ([("foo", "bar"), ("baz", "bin")], [])
-                    result1
-
-        let ctype2 = "multipart/form-data; boundary=AaB03x"
-        result2 <- parseRequestBody' lbsBackEnd $ toRequest ctype2 content2
-        let expectedsmap2 =
-              [ ("title", "A File")
-              , ("summary", "This is my file\nfile test")
-              ]
-        let textPlain = "text/plain; charset=iso-8859-1"
-        let expectedfile2 =
-              [("document", FileInfo "b.txt" textPlain "This is a file.\nIt has two lines.")]
-        let expected2 = (expectedsmap2, expectedfile2)
-        assertEqual "parsing post multipart/form-data"
-                    expected2
-                    result2
-
-        let ctype3 = "multipart/form-data; boundary=----WebKitFormBoundaryB1pWXPZ6lNr8RiLh"
-        result3 <- parseRequestBody' lbsBackEnd $ toRequest ctype3 content3
-        let expectedsmap3 = []
-        let expectedfile3 = [("yaml", FileInfo "README" "application/octet-stream" "Photo blog using Hack.\n")]
-        let expected3 = (expectedsmap3, expectedfile3)
-        assertEqual "parsing actual post multipart/form-data"
-                    expected3
-                    result3
-
-        result2' <- parseRequestBody' lbsBackEnd $ toRequest' ctype2 content2
-        assertEqual "parsing post multipart/form-data 2"
-                    expected2
-                    result2'
-
-        result3' <- parseRequestBody' lbsBackEnd $ toRequest' ctype3 content3
-        assertEqual "parsing actual post multipart/form-data 2"
-                    expected3
-                    result3'
+         "--AaB03x\n"
+      <> "Content-Disposition: form-data; name=\"document\"; filename=\"b.txt\"\n"
+      <> "Content-Type: text/plain; charset=iso-8859-1\n\n"
+      <> "This is a file.\n"
+      <> "It has two lines.\n"
+      <> "--AaB03x\n"
+      <> "Content-Disposition: form-data; name=\"title\"\n"
+      <> "Content-Type: text/plain; charset=iso-8859-1\n\n"
+      <> "A File\n"
+      <> "--AaB03x\n"
+      <> "Content-Disposition: form-data; name=\"summary\"\n"
+      <> "Content-Type: text/plain; charset=iso-8859-1\n\n"
+      <> "This is my file\n"
+      <> "file test\n"
+      <> "--AaB03x--"
+    content3 =
+         "------WebKitFormBoundaryB1pWXPZ6lNr8RiLh\r\n"
+      <> "Content-Disposition: form-data; name=\"yaml\"; filename=\"README\"\r\n"
+      <> "Content-Type: application/octet-stream\r\n\r\n"
+      <> "Photo blog using Hack.\n\r\n"
+      <> "------WebKitFormBoundaryB1pWXPZ6lNr8RiLh--\r\n"
 
 caseMultipartPlus :: Assertion
 caseMultipartPlus = do
