03276c2cc3570fb09ce61d7b6cf2bd24fd863b92
Marcin Tolysz <tolysz@gmail.com>
2015-03-18 16:36:22 +0000

    Update Mime.hs
    Hi, there are fonts which already use WOFF2 standard.
    http://dev.w3.org/webfonts/WOFF2/spec/#IMT

--- mime-types/Network/Mime.hs
+++ mime-types/Network/Mime.hs
@@ -1000,6 +1000,7 @@ defaultMimeMap = Map.fromAscList [
     , ("wmx", "video/x-ms-wmx")
     , ("wmz", "application/x-msmetafile")
     , ("woff", "application/font-woff")
+    , ("woff2", "application/font-woff2")
     , ("wpd", "application/vnd.wordperfect")
     , ("wpl", "application/vnd.ms-wpl")
     , ("wps", "application/vnd.ms-works")
