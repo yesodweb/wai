ae095af5ec56b9f2c28ff3c28b94a0a1a1410dcb
Michael Snoyman <michael@fpcomplete.com>
2015-07-10 04:54:47 +0300

    Add stack support to doctests

--- warp/test/doctests.hs
+++ warp/test/doctests.hs
@@ -1,11 +1,18 @@
 module Main where
 
 import Test.DocTest
+import System.Environment (getEnvironment)
 
 main :: IO ()
-main = doctest [
-    "-idist/build/autogen/"
-  , "-optP-include"
-  , "-optPdist/build/autogen/cabal_macros.h"
-  , "Network/Wai/Handler/Warp.hs"
-  ]
+main = do
+    env <- getEnvironment
+    let dist =
+            case lookup "HASKELL_DIST_DIR" env of
+                Nothing -> "dist"
+                Just x -> x
+    doctest
+        [ concat ["-i", dist, "/build/autogen/"]
+        , "-optP-include"
+        , concat ["-optP", dist, "/build/autogen/cabal_macros.h"]
+        , "Network/Wai/Handler/Warp.hs"
+        ]
