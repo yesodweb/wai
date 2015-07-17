52f892c3c9643a7e0f868832c2d783f3678c980d
Michael Snoyman <michael@fpcomplete.com>
2015-07-18 02:15:38 +0300

    stack.yaml file, doctest upgrade

--- warp/test/doctests.hs
+++ warp/test/doctests.hs
@@ -1,18 +1,4 @@
-module Main where
-
 import Test.DocTest
-import System.Environment (getEnvironment)
 
 main :: IO ()
-main = do
-    env <- getEnvironment
-    let dist =
-            case lookup "HASKELL_DIST_DIR" env of
-                Nothing -> "dist"
-                Just x -> x
-    doctest
-        [ concat ["-i", dist, "/build/autogen/"]
-        , "-optP-include"
-        , concat ["-optP", dist, "/build/autogen/cabal_macros.h"]
-        , "Network/Wai/Handler/Warp.hs"
-        ]
+main = doctest ["Network"]
