8574d5722346fed63fcaca634e591648170a16fd
Andrew Pritchard <awpr@google.com>
2015-07-30 21:31:09 -0700

    Add alias of Request for HTTP2

--- wai/Network/Wai/HTTP2.hs
+++ wai/Network/Wai/HTTP2.hs
@@ -11,18 +11,24 @@ module Network.Wai.HTTP2
 import           Blaze.ByteString.Builder     (Builder)
 import qualified Network.HTTP.Types as H
 
-import Network.Wai.Internal (Request)
+import qualified Network.Wai.Internal as H1 (Request)
 
 -- | Headers sent after the end of a data stream, as defined by section 4.1.2 of
 -- the HTTP\/1.1 spec (RFC 7230), and section 8.1 of the HTTP\/2 spec.
 type Trailers = H.ResponseHeaders
 
+-- | The type of an HTTP\/2 request.
+-- TODO(awpr): specialize this for HTTP\/2.
+type Request = H1.Request
+
 -- | The HTTP\/2-aware equivalent of 'Network.Wai.Application'.
-type Http2Application = Request -> (forall a. Response a -> IO a) -> IO Trailers
+type Http2Application = Request -> Responder
 
 type Body a = (Builder -> IO ()) -> IO () -> IO a
 
 type Response a = (H.Status, H.ResponseHeaders, Body a)
+
+type Responder = (forall a. Response a -> IO a) -> IO Trailers
 
 responseStatus :: Response a -> H.Status
 responseStatus (s, _, _) = s
