0b1671fd295e86bb0d887e61386c80deca7235a6
Michael Snoyman <michael@snoyman.com>
2015-12-07 10:42:24 +0200

    Combo Stack and Cabal builds in Travis

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -2,6 +2,19 @@
 
 set -eux
 
+# For simplicity, installing cabal-meta and Stack for both the Cabal and Stack
+# build plans, even though they're never both necessary.
+
+mkdir -p ~/.local/bin
+if [ `uname` = "Darwin" ]
+then
+  curl -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin
+  brew install fcgi
+else
+  curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+  curl -L http://download.fpcomplete.com/michael/cabal-meta.tar.gz | tar xz -C $HOME/.local/bin
+fi
+
 mkdir -p $HOME/.cabal
 cat > $HOME/.cabal/config <<EOF
 remote-repo: hackage.haskell.org:http://hackage.fpcomplete.com/
