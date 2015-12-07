47ed0cc45fbd6083f94e6a96f5cb47618ba2a25b
Kazu Yamamoto <kazu@iij.ad.jp>
2015-12-07 12:57:07 +0900

    avoiding using fromJust.

--- warp/Network/Wai/Handler/Warp/File.hs
+++ warp/Network/Wai/Handler/Warp/File.hs
@@ -14,7 +14,6 @@ import qualified Data.ByteString as B hiding (pack)
 import qualified Data.ByteString.Builder as B
 import qualified Data.ByteString.Char8 as B (pack, readInteger)
 import qualified Data.ByteString.Lazy as L
-import Data.Maybe
 import Network.HTTP.Date
 import qualified Network.HTTP.Types as H
 import qualified Network.HTTP.Types.Header as H
@@ -34,7 +33,7 @@ data RspFileInfo = WithoutBody H.Status
 conditionalRequest :: I.FileInfo
                    -> H.ResponseHeaders -> IndexedHeader
                    -> RspFileInfo
-conditionalRequest finfo hs0 reqidx = case fromJust mx of
+conditionalRequest finfo hs0 reqidx = case condition of
     nobody@(WithoutBody _) -> nobody
     WithBody s _ off len   -> let hs = (H.hLastModified,date) :
                                        addContentHeaders hs0 off len size
@@ -43,10 +42,12 @@ conditionalRequest finfo hs0 reqidx = case fromJust mx of
     mtime = I.fileInfoTime finfo
     size  = I.fileInfoSize finfo
     date  = I.fileInfoDate finfo
-    mx = ifmodified    reqidx size mtime
-                         <|> ifunmodified  reqidx size mtime
-                         <|> ifrange       reqidx size mtime
-                         <|> unconditional reqidx size mtime
+    mcondition = ifmodified    reqidx size mtime
+             <|> ifunmodified  reqidx size mtime
+             <|> ifrange       reqidx size mtime
+    condition = case mcondition of
+        Nothing -> unconditional reqidx size
+        Just x  -> x
 
 ----------------------------------------------------------------
 
@@ -64,43 +65,43 @@ ifRange reqidx = reqidx ! idxIfRange >>= parseHTTPDate
 ifmodified :: IndexedHeader -> Integer -> HTTPDate -> Maybe RspFileInfo
 ifmodified reqidx size mtime = do
     date <- ifModifiedSince reqidx
-    if date /= mtime
-       then unconditional reqidx size mtime
-       else Just $ WithoutBody H.notModified304
+    return $ if date /= mtime
+             then unconditional reqidx size
+             else  WithoutBody H.notModified304
 
 ifunmodified :: IndexedHeader -> Integer -> HTTPDate -> Maybe RspFileInfo
 ifunmodified reqidx size mtime = do
     date <- ifUnmodifiedSince reqidx
-    if date == mtime
-       then unconditional reqidx size mtime
-       else Just $ WithoutBody H.preconditionFailed412
+    return $ if date == mtime
+             then unconditional reqidx size
+             else WithoutBody H.preconditionFailed412
 
 ifrange :: IndexedHeader -> Integer -> HTTPDate -> Maybe RspFileInfo
 ifrange reqidx size mtime = do
     date <- ifRange reqidx
     rng  <- reqidx ! idxRange
-    if date == mtime
-       then parseRange rng size
-       else Just $ WithBody H.ok200 [] 0 size
+    return $ if date == mtime
+             then parseRange rng size
+             else WithBody H.ok200 [] 0 size
 
-unconditional :: IndexedHeader -> Integer -> HTTPDate -> Maybe RspFileInfo
-unconditional reqidx size _ = case reqidx ! idxRange of
-    Nothing  -> Just $ WithBody H.ok200 [] 0 size
+unconditional :: IndexedHeader -> Integer -> RspFileInfo
+unconditional reqidx size = case reqidx ! idxRange of
+    Nothing  -> WithBody H.ok200 [] 0 size
     Just rng -> parseRange rng size
 
 ----------------------------------------------------------------
 
-parseRange :: ByteString -> Integer -> Maybe RspFileInfo
+parseRange :: ByteString -> Integer -> RspFileInfo
 parseRange rng size = case parseByteRanges rng of
-    Nothing    -> Just $ WithoutBody H.requestedRangeNotSatisfiable416
-    Just []    -> Just $ WithoutBody H.requestedRangeNotSatisfiable416
+    Nothing    -> WithoutBody H.requestedRangeNotSatisfiable416
+    Just []    -> WithoutBody H.requestedRangeNotSatisfiable416
     Just (r:_) -> let (!beg, !end) = checkRange r size
                       !len = end - beg + 1
                       s = if beg == 0 && end == size - 1 then
                               H.ok200
                             else
                               H.partialContent206
-                  in Just $ WithBody s [] beg len
+                  in WithBody s [] beg len
 
 checkRange :: H.ByteRange -> Integer -> (Integer, Integer)
 checkRange (H.ByteRangeFrom   beg)     size = (beg, size - 1)
