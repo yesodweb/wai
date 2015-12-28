3fb5bd1f3b5c04e043bd2e63c322716a99a17642
Michael Snoyman <michael@snoyman.com>
2015-12-28 11:08:57 +0200

    Allow insecure Stack download for Mac on Travis

--- .travis-setup.sh
+++ .travis-setup.sh
@@ -8,7 +8,7 @@ set -eux
 mkdir -p ~/.local/bin
 if [ `uname` = "Darwin" ]
 then
-  curl -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin
+  curl --allow-insecure -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 --include '*/stack' -C ~/.local/bin
   brew install fcgi
 else
   curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
