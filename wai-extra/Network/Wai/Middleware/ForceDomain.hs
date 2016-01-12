a46f52a42706abfe27391b55deb3886c935d235a
Michael Snoyman <michael@snoyman.com>
2016-01-12 13:21:30 +0200

    Remove trailing whitespace

--- wai-extra/Network/Wai/Middleware/ForceDomain.hs
+++ wai-extra/Network/Wai/Middleware/ForceDomain.hs
@@ -11,13 +11,13 @@ import Prelude
 import Network.Wai
 import Network.Wai.Request
 
--- | Force a domain by redirecting. 
--- The `checkDomain` function takes the current domain and checks whether it is correct. 
--- It should return `Nothing` if the domain is correct, or `Just "domain.com"` if it is incorrect. 
+-- | Force a domain by redirecting.
+-- The `checkDomain` function takes the current domain and checks whether it is correct.
+-- It should return `Nothing` if the domain is correct, or `Just "domain.com"` if it is incorrect.
 --
 -- @since 3.0.14
 forceDomain :: (ByteString -> Maybe ByteString) -> Middleware
-forceDomain checkDomain app req sendResponse = 
+forceDomain checkDomain app req sendResponse =
     case requestHeaderHost req >>= checkDomain of
         Nothing ->
             app req sendResponse
@@ -26,10 +26,10 @@ forceDomain checkDomain app req sendResponse =
 
     where
         -- From: Network.Wai.Middleware.ForceSSL
-        redirectResponse domain = 
+        redirectResponse domain =
             responseBuilder status [(hLocation, location domain)] mempty
 
-        location h = 
+        location h =
             let p = if appearsSecure req then "https://" else "http://" in
             p <> h <> rawPathInfo req <> rawQueryString req
 
