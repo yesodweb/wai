16a713d55c8ce011e8586b7e95c5722c308bf026
Kazu Yamamoto <kazu@iij.ad.jp>
2020-05-18 09:46:42 +0900

    implementing multi LRU cache.

--- wai-http2-extra/Network/Wai/Middleware/Push/Referer/LRU.hs
+++ wai-http2-extra/Network/Wai/Middleware/Push/Referer/LRU.hs
@@ -1,54 +1,58 @@
 -- from https://jaspervdj.be/posts/2015-02-24-lru-cache.html
 module Network.Wai.Middleware.Push.Referer.LRU where
 
-import qualified Data.OrdPSQ as OrdPSQ
+import Data.OrdPSQ (OrdPSQ)
+import qualified Data.OrdPSQ as PSQ
 import Data.Int (Int64)
 import Data.Maybe (isNothing)
 import Prelude hiding (lookup)
+
+import Network.Wai.Middleware.Push.Referer.Multi (Multi)
+import qualified Network.Wai.Middleware.Push.Referer.Multi as M
 
 type Priority = Int64
 
 data Cache k v = Cache {
     cCapacity :: Int       -- ^ The maximum number of elements in the queue
   , cSize     :: Int       -- ^ The current number of elements in the queue
+  , cValLimit :: Int
   , cTick     :: Priority  -- ^ The next logical time
-  , cQueue    :: OrdPSQ.OrdPSQ k Priority v
+  , cQueue    :: OrdPSQ k Priority (Multi v)
   } deriving (Eq, Show)
 
-empty :: Int -> Cache k v
-empty capacity
+empty :: Int -> Int -> Cache k v
+empty capacity valLimit
   | capacity < 1 = error "Cache.empty: capacity < 1"
   | otherwise    = Cache {
         cCapacity = capacity
       , cSize     = 0
+      , cValLimit = valLimit
       , cTick     = 0
-      , cQueue    = OrdPSQ.empty
+      , cQueue    = PSQ.empty
       }
 
 trim :: Ord k => Cache k v -> Cache k v
 trim c
-  | cTick c == maxBound  = empty (cCapacity c)
+  | cTick c == maxBound  = empty (cCapacity c) (cValLimit c)
   | cSize c > cCapacity c = c {
         cSize  = cSize c - 1
-      , cQueue = OrdPSQ.deleteMin (cQueue c)
+      , cQueue = PSQ.deleteMin (cQueue c)
       }
   | otherwise             = c
 
-insert :: Ord k => k -> v -> Cache k v -> Cache k v
-insert key val c = trim c'
+insert :: (Ord k, Ord v) => k -> v -> Cache k v -> Cache k v
+insert k v c = case PSQ.alter lookupAndBump k (cQueue c) of
+    (True,  q) -> trim $ c { cTick = cTick c + 1, cQueue = q, cSize = cSize c + 1}
+    (False, q) -> trim $ c { cTick = cTick c + 1, cQueue = q }
   where
-    (mbOldVal, queue) = OrdPSQ.insertView key (cTick c) val (cQueue c)
-    c'= c {
-        cSize  = if isNothing mbOldVal then cSize c + 1 else cSize c
-      , cTick  = cTick c + 1
-      , cQueue = queue
-      }
+    lookupAndBump Nothing       = (True,  Just (cTick c, M.singleton (cValLimit c) v))
+    lookupAndBump (Just (_, x)) = (False, Just (cTick c, M.insert v x))
 
-lookup :: Ord k => k -> Cache k v -> Maybe (v, Cache k v)
-lookup k c = case OrdPSQ.alter lookupAndBump k (cQueue c) of
+lookup :: Ord k => k -> Cache k v -> Maybe ([v], Cache k v)
+lookup k c = case PSQ.alter lookupAndBump k (cQueue c) of
     (Nothing, _) -> Nothing
     (Just x, q)  -> let c' = trim $ c { cTick = cTick c + 1, cQueue = q }
-                    in Just (x, c')
+                    in Just (M.list x, c')
   where
     lookupAndBump Nothing       = (Nothing, Nothing)
-    lookupAndBump (Just (_, x)) = (Just x,  Just ((cTick c), x))
+    lookupAndBump (Just (_, x)) = (Just x,  Just (cTick c, x))
