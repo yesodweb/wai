92151059e34166414ae090b0bf9edfb599b91882
Tillmann Vogt <tillk.vogt@gmail.com>
2021-06-25 12:38:49 +0200

    Internal module to get access to certSettings

--- warp-tls/Network/Wai/Handler/WarpTLS.hs
+++ warp-tls/Network/Wai/Handler/WarpTLS.hs
@@ -76,122 +76,14 @@ import qualified Network.TLS.SessionManager as SM
 import Network.Wai (Application)
 import Network.Wai.Handler.Warp
 import Network.Wai.Handler.Warp.Internal
+import Network.Wai.Handler.WarpTLS.Internal
 import System.IO.Error (isEOFError, ioeGetErrorType)
-
-----------------------------------------------------------------
-
--- | Determines where to load the certificate, chain 
--- certificates, and key from.
-data CertSettings 
-  = CertFromFile !FilePath ![FilePath] !FilePath
-  | CertFromMemory !S.ByteString ![S.ByteString] !S.ByteString
-  | CertFromRef !(I.IORef S.ByteString) ![I.IORef S.ByteString] !(I.IORef S.ByteString)
 
 -- | The default 'CertSettings'.
 defaultCertSettings :: CertSettings
 defaultCertSettings = CertFromFile "certificate.pem" [] "key.pem"
 
 ----------------------------------------------------------------
-
--- | Settings for WarpTLS.
-data TLSSettings = TLSSettings {
-    certSettings :: CertSettings
-    -- ^ Where are the certificate, chain certificates, and key
-    -- loaded from?
-    --
-    -- >>> certSettings defaultTlsSettings
-    -- tlsSettings "certificate.pem" "key.pem"
-    -- 
-    -- @since 3.3.0
-  , onInsecure :: OnInsecure
-    -- ^ Do we allow insecure connections with this server as well?
-    --
-    -- >>> onInsecure defaultTlsSettings
-    -- DenyInsecure "This server only accepts secure HTTPS connections."
-    --
-    -- Since 1.4.0
-  , tlsLogging :: TLS.Logging
-    -- ^ The level of logging to turn on.
-    --
-    -- Default: 'TLS.defaultLogging'.
-    --
-    -- Since 1.4.0
-  , tlsAllowedVersions :: [TLS.Version]
-#if MIN_VERSION_tls(1,5,0)
-    -- ^ The TLS versions this server accepts.
-    --
-    -- >>> tlsAllowedVersions defaultTlsSettings
-    -- [TLS13,TLS12,TLS11,TLS10]
-    --
-    -- Since 1.4.2
-#else
-    -- ^ The TLS versions this server accepts.
-    --
-    -- >>> tlsAllowedVersions defaultTlsSettings
-    -- [TLS12,TLS11,TLS10]
-    --
-    -- Since 1.4.2
-#endif
-  , tlsCiphers :: [TLS.Cipher]
-#if MIN_VERSION_tls(1,5,0)
-    -- ^ The TLS ciphers this server accepts.
-    --
-    -- >>> tlsCiphers defaultTlsSettings
-    -- [ECDHE-ECDSA-AES256GCM-SHA384,ECDHE-ECDSA-AES128GCM-SHA256,ECDHE-RSA-AES256GCM-SHA384,ECDHE-RSA-AES128GCM-SHA256,DHE-RSA-AES256GCM-SHA384,DHE-RSA-AES128GCM-SHA256,ECDHE-ECDSA-AES256CBC-SHA384,ECDHE-RSA-AES256CBC-SHA384,DHE-RSA-AES256-SHA256,ECDHE-ECDSA-AES256CBC-SHA,ECDHE-RSA-AES256CBC-SHA,DHE-RSA-AES256-SHA1,RSA-AES256GCM-SHA384,RSA-AES256-SHA256,RSA-AES256-SHA1,AES128GCM-SHA256,AES256GCM-SHA384]
-    --
-    -- Since 1.4.2
-#else
-    -- ^ The TLS ciphers this server accepts.
-    --
-    -- >>> tlsCiphers defaultTlsSettings
-    -- [ECDHE-ECDSA-AES256GCM-SHA384,ECDHE-ECDSA-AES128GCM-SHA256,ECDHE-RSA-AES256GCM-SHA384,ECDHE-RSA-AES128GCM-SHA256,DHE-RSA-AES256GCM-SHA384,DHE-RSA-AES128GCM-SHA256,ECDHE-ECDSA-AES256CBC-SHA384,ECDHE-RSA-AES256CBC-SHA384,DHE-RSA-AES256-SHA256,ECDHE-ECDSA-AES256CBC-SHA,ECDHE-RSA-AES256CBC-SHA,DHE-RSA-AES256-SHA1,RSA-AES256GCM-SHA384,RSA-AES256-SHA256,RSA-AES256-SHA1]
-    --
-    -- Since 1.4.2
-#endif
-  , tlsWantClientCert :: Bool
-    -- ^ Whether or not to demand a certificate from the client.  If this
-    -- is set to True, you must handle received certificates in a server hook
-    -- or all connections will fail.
-    --
-    -- >>> tlsWantClientCert defaultTlsSettings
-    -- False
-    --
-    -- Since 3.0.2
-  , tlsServerHooks :: TLS.ServerHooks
-    -- ^ The server-side hooks called by the tls package, including actions
-    -- to take when a client certificate is received.  See the "Network.TLS"
-    -- module for details.
-    --
-    -- Default: def
-    --
-    -- Since 3.0.2
-  , tlsServerDHEParams :: Maybe DH.Params
-    -- ^ Configuration for ServerDHEParams
-    -- more function lives in `cryptonite` package
-    --
-    -- Default: Nothing
-    --
-    -- Since 3.2.2
-  , tlsSessionManagerConfig :: Maybe SM.Config
-    -- ^ Configuration for in-memory TLS session manager.
-    -- If Nothing, 'TLS.noSessionManager' is used.
-    -- Otherwise, an in-memory TLS session manager is created
-    -- according to 'Config'.
-    --
-    -- Default: Nothing
-    --
-    -- Since 3.2.4
-  , tlsCredentials :: Maybe TLS.Credentials
-    -- ^ Specifying 'TLS.Credentials' directly.  If this value is
-    --   specified, other fields such as 'certFile' are ignored.
-    --
-    --   Since 3.2.12
-  , tlsSessionManager :: Maybe TLS.SessionManager
-    -- ^ Specifying 'TLS.SessionManager' directly. If this value is
-    --   specified, 'tlsSessionManagerConfig' is ignored.
-    --
-    --   Since 3.2.12
-  }
 
 -- | Default 'TLSSettings'. Use this to create 'TLSSettings' with the field record name (aka accessors).
 defaultTlsSettings :: TLSSettings
@@ -216,13 +108,6 @@ defaultTlsSettings = TLSSettings {
 -- taken from stunnel example in tls-extra
 ciphers :: [TLS.Cipher]
 ciphers = TLSExtra.ciphersuite_strong
-
-----------------------------------------------------------------
-
--- | An action when a plain HTTP comes to HTTP over TLS/SSL port.
-data OnInsecure = DenyInsecure L.ByteString
-                | AllowInsecure
-                deriving (Show)
 
 ----------------------------------------------------------------
 
@@ -296,12 +181,6 @@ tlsSettingsChainRef
 tlsSettingsChainRef cert chainCerts key = defaultTlsSettings { 
     certSettings = CertFromRef cert chainCerts key
   }
-
-
--- Since 3.3.1
--- | Some programs need access to cert settings
-getCertSettings :: TLSSettings -> CertSettings
-getCertSettings tlsSetgs = certSettings tlsSetgs
 
 ----------------------------------------------------------------
 
