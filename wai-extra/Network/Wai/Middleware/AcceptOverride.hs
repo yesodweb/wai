b924a520f49845312884ec65d626f124a1e603fa
Felix Paulusma <felix.paulusma@gmail.com>
2022-04-26 18:27:18 +0100

    wai-extra/acceptOverride: added documentation

--- wai-extra/Network/Wai/Middleware/AcceptOverride.hs
+++ wai-extra/Network/Wai/Middleware/AcceptOverride.hs
@@ -1,10 +1,27 @@
 module Network.Wai.Middleware.AcceptOverride
-    ( acceptOverride
+    ( -- $howto
+      acceptOverride
     ) where
 
 import Control.Monad (join)
 import Data.ByteString (ByteString)
 import Network.Wai
+
+-- $howto
+-- This 'Middleware' provides a way for the request itself to
+-- tell the server to override the \"Accept\" header by looking
+-- for the \"_accept\" query parameter in the query string and
+-- inserting or replacing the \"Accept\" header with that string.
+--
+-- For example:
+--
+-- @
+-- ?_accept=SomeValue
+-- @
+--
+-- This will result in \"Accept: SomeValue\" being set in the
+-- request as a header, and all other previous \"Accept\" headers
+-- will be removed from the request.
 
 acceptOverride :: Middleware
 acceptOverride app req =
