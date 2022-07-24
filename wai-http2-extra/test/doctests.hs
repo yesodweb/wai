69e253dbf609d1a339c2cad4c09663acef4b2340
Felix Paulusma <felix.paulusma@gmail.com>
2022-07-24 03:59:32 +0200

    make sure cgi is found for lts-16 and don't run doctest for wai-http2-extra when GHC >= 9.0

--- wai-http2-extra/test/doctests.hs
+++ wai-http2-extra/test/doctests.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE CPP  #-}
 import Test.DocTest
 
 main :: IO ()
-main = doctest ["Network"]
+main =
+#if __GLASGOW_HASKELL__ < 900
+    doctest ["Network"]
+#else
+    putStrLn "Only works on GHC < 9.0"
+#endif
