41ecdf5baa175d15e581b65682ed53a5c931c728
Felix Paulusma <felix.paulusma@gmail.com>
2022-08-21 19:37:53 +0200

    wai-extra: add/adjust documentation of 'CombineHeaders'

--- wai-extra/Network/Wai/Middleware/CombineHeaders.hs
+++ wai-extra/Network/Wai/Middleware/CombineHeaders.hs
@@ -1,5 +1,18 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RecordWildCards #-}
+{- |
+Sometimes incoming requests don't stick to the
+"no duplicate headers" invariant, for a number
+of possible reasons (e.g. proxy servers blindly
+adding headers), or your application (or other
+middleware) blindly adds headers.
+
+In those cases, you can use this 'Middleware'
+to make sure that headers that /can/ be combined
+/are/ combined. (e.g. applications might only
+check the first \"Accept\" header and fail, while
+there might be another one that would match)
+-}
 module Network.Wai.Middleware.CombineHeaders
     ( combineHeaders
     , defaultCombineSettings
@@ -16,7 +29,19 @@ import qualified Network.HTTP.Types.Header as H
 import Network.Wai (Middleware, requestHeaders, mapResponseHeaders)
 import Network.Wai.Header (dropWhileEnd)
 
--- |
+-- | These settings define which headers should be combined
+-- ('combineHeaderMap'), if the combining should happen on
+-- incoming headers ('combineRequestHeaders') and if it
+-- should happen on outgoing headers ('combineResponseHeaders').
+--
+-- Any header you put in the 'combineHeaderMap' *will* be
+-- used to combine those headers with commas. There's no
+-- check to see if it is a header that allows comma-separated
+-- lists, so if you want to combine custom headers, go ahead.
+--
+-- (You can check the documentation of 'defaultCombineSettings'
+-- to see which standard headers are specified to be able to be
+-- combined)
 --
 -- @since 3.1.13.0
 data CombineSettings = CombineSettings {
@@ -28,9 +53,44 @@ data CombineSettings = CombineSettings {
     -- ^ Should response headers be combined?
 } deriving (Eq, Show)
 
--- | Settings that combine request headers, but don't touch response headers.
+-- | Settings that combine request headers,
+-- but don't touch response headers.
 --
--- All types of headers that /can/ be combined /will/ be combined.
+-- All types of headers that /can/ be combined
+-- (as defined in the spec) /will/ be combined.
+--
+-- To be exact, this is the list:
+--
+-- * \"Accept\"
+-- * \"Accept-CH\"
+-- * \"Accept-Charset\"
+-- * \"Accept-Encoding\"
+-- * \"Accept-Language\"
+-- * \"Accept-Post\"
+-- * \"Access-Control-Allow-Headers\"
+-- * \"Access-Control-Allow-Methods\"
+-- * \"Access-Control-Expose-Headers\"
+-- * \"Access-Control-Request-Headers\"
+-- * \"Allow\"
+-- * \"Alt-Svc\"
+-- * \"Cache-Control\"
+-- * \"Clear-Site-Data\"
+-- * \"Connection\"
+-- * \"Content-Encoding\"
+-- * \"Content-Language\"
+-- * \"Digest\"
+-- * \"If-Match\"
+-- * \"If-None-Match\"
+-- * \"Link\"
+-- * \"Permissions-Policy\"
+-- * \"TE\"
+-- * \"Timing-Allow-Origin\"
+-- * \"Trailer\"
+-- * \"Transfer-Encoding\"
+-- * \"Upgrade\"
+-- * \"Via\"
+-- * \"Vary\"
+-- * \"Want-Digest\"
 --
 -- @since 3.1.13.0
 defaultCombineSettings :: CombineSettings
@@ -40,17 +100,16 @@ defaultCombineSettings = CombineSettings {
     combineResponseHeaders = False
 }
 
--- |
--- This middleware will reorganize the incoming headers in
--- such a way that it combines any duplicates of headers
--- that, on their own, can normally have more than one value,
--- and any other headers will stay untouched.
+-- | This middleware will reorganize the incoming and/or outgoing
+-- headers in such a way that it combines any duplicates of
+-- headers that, on their own, can normally have more than one
+-- value, and any other headers will stay untouched.
 --
 -- This middleware WILL change the global order of headers
 -- (they will be put in alphabetical order), but keep the
 -- order of the same type of header. I.e. if there are 3
--- \"Cookie\" headers, the first one will still be first, the
--- second one will still be second, etc. But now they are
+-- \"Set-Cookie\" headers, the first one will still be first,
+-- the second one will still be second, etc. But now they are
 -- guaranteed to be next to each other.
 --
 -- N.B. This 'Middleware' assumes the headers it combines
@@ -95,13 +154,22 @@ finishHeaders name (shouldCombine, xs) hdrs =
     combinedHeader (KeepOnly val)
         | val `elem` xs = val
         | otherwise = combineHdrs xs
+    -- headers were reversed, so do 'reverse' before combining
     combineHdrs = B.intercalate ", " . fmap clean . L.reverse
     clean = dropWhileEnd $ \w -> w == _comma || w == _space
 
 type HeaderHandling = (Maybe HandleType, [B.ByteString])
 
 -- | Both will concatenate with @,@ (commas), but 'KeepOnly' will
--- drop all headers except the given one (in case of wildcards/special values)
+-- drop all values except the given one (in case of wildcards/special values)
+--
+-- For example: If there are multiple @"Clear-Site-Data"@ headers, but one of
+-- them is the wildcard @\"*\"@ value, using @'KeepOnly' "*"@ will cause all
+-- others to be dropped and only the wildcard value to remain.
+-- (The @\"*\"@ wildcard in this case means /ALL site data/ should be cleared,
+-- so no need to include more)
+--
+-- @since 3.1.13.0
 data HandleType
     = Regular
     | KeepOnly B.ByteString
