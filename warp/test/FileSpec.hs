c96ad0b93240f6018c86f598f320495c92f429a4
Felix Paulusma <felix.paulusma@gmail.com>
2023-10-05 20:52:23 +0200

    warp/test: added more tests for 'FileSpec' to verify 'If-(None-)Match' and 'If-(Un)Modified-Since' headers interact as expected.

--- warp/test/FileSpec.hs
+++ warp/test/FileSpec.hs
@@ -2,6 +2,8 @@
 
 module FileSpec (main, spec) where
 
+import Data.ByteString
+import Data.String (fromString)
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp.File
 import Network.Wai.Handler.Warp.FileInfoCache
@@ -12,34 +14,96 @@ import Test.Hspec
 main :: IO ()
 main = hspec spec
 
+changeHeaders :: (ResponseHeaders -> ResponseHeaders) -> RspFileInfo -> RspFileInfo
+changeHeaders f rfi =
+    case rfi of
+        WithBody s hs off len -> WithBody s (f hs) off len
+        other -> other
+
+getHeaders :: RspFileInfo -> ResponseHeaders
+getHeaders rfi =
+    case rfi of
+        WithBody _ hs _ _ -> hs
+        _ -> []
+
 testFileRange :: String
-              -> RequestHeaders -> FilePath
+              -> RequestHeaders
               -> RspFileInfo
               -> Spec
-testFileRange desc reqhs file ans = it desc $ do
-    finfo <- getInfo file
-    let WithBody s hs off len = ans
-        hs' = ("Last-Modified",fileInfoDate finfo) : hs
-        ans' = WithBody s hs' off len
-    conditionalRequest finfo [] (indexResponseHeader hs) (indexRequestHeader reqhs) `shouldBe` ans'
+testFileRange desc reqhs ans = it desc $ do
+    finfo <- getInfo "attic/hex"
+    let f = (:) ("Last-Modified", fileInfoDate finfo)
+        hs = getHeaders ans
+        ans' = changeHeaders f ans
+    conditionalRequest
+        finfo
+        []
+        methodGet
+        (indexResponseHeader hs)
+        (indexRequestHeader reqhs) `shouldBe` ans'
+
+farPast, farFuture :: ByteString
+farPast = "Thu, 01 Jan 1970 00:00:00 GMT"
+farFuture = "Sun, 05 Oct 3000 00:00:00 GMT"
+
+regularBody :: RspFileInfo
+regularBody = WithBody ok200 [("Content-Length","16"),("Accept-Ranges","bytes")] 0 16
+
+make206Body :: Integer -> Integer -> RspFileInfo
+make206Body start len =
+    WithBody status206 [crHeader, lenHeader, ("Accept-Ranges","bytes")] start len
+  where
+    lenHeader = ("Content-Length", fromString $ show len)
+    crHeader = ("Content-Range", fromString $ "bytes " <> show start <> "-" <> show (start + len - 1) <> "/16")
 
 spec :: Spec
 spec = do
     describe "conditionalRequest" $ do
         testFileRange
             "gets a file size from file system"
-            [] "attic/hex"
-            $ WithBody ok200 [("Content-Length","16"),("Accept-Ranges","bytes")] 0 16
+            []
+            regularBody
         testFileRange
             "gets a file size from file system and handles Range and returns Partical Content"
-            [("Range","bytes=2-14")] "attic/hex"
-            $ WithBody status206 [("Content-Range","bytes 2-14/16"),("Content-Length","13"),("Accept-Ranges","bytes")] 2 13
+            [("Range","bytes=2-14")]
+            $ make206Body 2 13
         testFileRange
             "truncates end point of range to file size"
-            [("Range","bytes=10-20")] "attic/hex"
-            $ WithBody status206 [("Content-Range","bytes 10-15/16"),("Content-Length","6"),("Accept-Ranges","bytes")] 10 6
+            [("Range","bytes=10-20")]
+            $ make206Body 10 6
         testFileRange
             "gets a file size from file system and handles Range and returns OK if Range means the entire"
-            [("Range:","bytes=0-15")] "attic/hex"
-            $ WithBody status200 [("Content-Length","16"),("Accept-Ranges","bytes")] 0 16
-
+            [("Range:","bytes=0-15")]
+            regularBody
+        testFileRange
+            "returns a 412 if the file has been changed in the meantime"
+            [("If-Unmodified-Since", farPast)]
+            $ WithoutBody status412
+        testFileRange
+            "gets a file if the file has not been changed in the meantime"
+            [("If-Unmodified-Since", farFuture)]
+            regularBody
+        testFileRange
+            "ignores the If-Unmodified-Since header if an If-Match header is also present"
+            [("If-Match", "SomeETag"), ("If-Unmodified-Since", farPast)]
+            regularBody
+        testFileRange
+            "still gives only a range, even after conditionals"
+            [("If-Match", "SomeETag"), ("If-Unmodified-Since", farPast), ("Range","bytes=10-20")]
+            $ make206Body 10 6
+        testFileRange
+            "gets a file if the file has been changed in the meantime"
+            [("If-Modified-Since", farPast)]
+            regularBody
+        testFileRange
+            "returns a 304 if the file has not been changed in the meantime"
+            [("If-Modified-Since", farFuture)]
+            $ WithoutBody status304
+        testFileRange
+            "ignores the If-Modified-Since header if an If-None-Match header is also present"
+            [("If-None-Match", "SomeETag"), ("If-Modified-Since", farFuture)]
+            regularBody
+        testFileRange
+            "still gives only a range, even after conditionals"
+            [("If-None-Match", "SomeETag"), ("If-Modified-Since", farFuture), ("Range","bytes=10-20")]
+            $ make206Body 10 6
