33a7123d631a0a08b83ad991d940e6fbccf913a9
Felix Paulusma <felix.paulusma@gmail.com>
2026-01-18 14:26:58 +0100

    time-manager: finished tests for 'System.TimeManager'

--- time-manager/test/Spec.hs
+++ time-manager/test/Spec.hs
@@ -52,32 +52,28 @@ main = hspec $ do
             _hndl <- registerKillThread defaultManager $ pure ()
             threadDelay 2000
 
-        let withHandleTest m expected = do
-                ref <- newIORef False
-                withHandle m (writeIORef ref True) $ \_hndl -> do
-                    threadDelay 2000
-                    b <- readIORef ref
-                    b `shouldBe` expected
-
         it "withHandle: registers timeout" $
-            withHandleTest mgr1 True
+            withHandleTest mgr1 $ \check _ -> do
+                threadDelay 2000
+                check True
 
         it "withHandle: doesn't register timeout" $
-            withHandleTest defaultManager False
+            withHandleTest defaultManager $ \check _ -> do
+                threadDelay 2000
+                check False
 
-        let withHandleKillTest m f = do
-                ref <- newIORef False
-                withHandleKillThread m (writeIORef ref True) $
-                    \_hndl -> f $ do
-                        threadDelay 2000
-                        b <- readIORef ref
-                        b `shouldBe` False
-
-        it "withHandleKillThread: registers timeout" $
-            withHandleKillTest mgr1 throwsTimeoutThread
+        -- We make a ref on the outside, to check that the ref is indeed
+        -- set before the timeout kills the action inside.
+        it "withHandleKillThread: registers timeout (and kills)" $ do
+            ref <- freshRef
+            withHandleKillTest (Just ref) mgr1 $ \_ _ ->
+                throwsTimeoutThread $ threadDelay 100
+            ref `refShouldBe` True
 
         it "withHandleKillThread: doesn't register timeout" $
-            withHandleKillTest defaultManager id
+            withHandleKillTest Nothing defaultManager $ \check _ -> do
+                threadDelay 200
+                check False
 
         it "cancel/pause works as expected" $ do
             m <- initialize 100
@@ -89,11 +85,64 @@ main = hspec $ do
             runIt cancel
             runIt pause
 
--- tickle
--- resume
+        it "tickle works as expected" $ do
+            m <- initialize 10_000
+            withHandleTest m $ \check hndl -> do
+                forM_ [(1 :: Int) .. 20] $ \_ -> do
+                    threadDelay 1000
+                    tickle hndl
+                check False
+
+        let runIt f = do
+                m <- initialize 10_000
+                void $ f =<< registerKillThread m (pure ())
+        it "resume works as expected" $ do
+            let runAndWaitForTimeout f =
+                    runIt $ \hndl -> do
+                        void $ f hndl
+                        threadDelay 20_000
+            -- Doing nothing kills the thread
+            throwsTimeoutThread . runAndWaitForTimeout $ \_ -> pure ()
+            -- Pausing stops the kill
+            runAndWaitForTimeout $ \hndl -> do
+                threadDelay 2500
+                pause hndl
+            -- Resuming kills the thread again
+            throwsTimeoutThread . runAndWaitForTimeout $ \hndl -> do
+                threadDelay 2500
+                pause hndl
+                threadDelay 20_000
+                resume hndl
+
+        -- "resuming" every 2.5ms 20 times
+        let testResume f = do
+                runIt $ \hndl -> do
+                    forM_ [(1 :: Int) .. 20] $ \_ -> do
+                        threadDelay 2500
+                        f hndl
+        it "resume also works as tickle" $
+            testResume resume
+
+        it "old resume did NOT work as tickle" $
+            throwsTimeoutThread $
+                testResume oldResume
+  where
+    withHandleTest = withTest withHandle Nothing
+    withHandleKillTest = withTest withHandleKillThread
+    -- Test that starts with a 'False' IORef and on timeout sets it to true
+    withTest withF mRef m f = do
+        ref <- maybe freshRef pure mRef
+        withF m (writeIORef ref True) . f $ refShouldBe ref
 
 mgr1 :: Manager
 mgr1 = Manager 1
+
+freshRef :: IO (IORef Bool)
+freshRef = newIORef False
+
+refShouldBe :: IORef Bool -> Bool -> IO ()
+refShouldBe ref expected =
+    readIORef ref >>= (`shouldBe` expected)
 
 throwsTimeoutThread :: IO () -> Expectation
 throwsTimeoutThread t = t `shouldThrow` (const True :: TimeoutThread -> Bool)
