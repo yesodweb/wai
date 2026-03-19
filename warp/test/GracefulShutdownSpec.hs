3c1b8c27e00e17b62f5c93e7732eb6c75868d6ff
Kazu Yamamoto <kazu@iij.ad.jp>
2026-03-19 14:00:42 +0900

    fourmolu

--- warp/test/GracefulShutdownSpec.hs
+++ warp/test/GracefulShutdownSpec.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ImpredicativeTypes #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NumericUnderscores #-}
-{-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module GracefulShutdownSpec (spec) where
 
@@ -9,6 +9,7 @@ import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Exception (bracket)
 import Control.Monad (void)
+import Data.Functor ((<&>))
 import Network.HTTP.Client
 import Network.HTTP.Types
 import Network.Socket (close)
@@ -16,7 +17,6 @@ import Network.Wai (responseLBS)
 import Network.Wai.Handler.Warp
 import System.Timeout (timeout)
 import Test.Hspec
-import Data.Functor ((<&>))
 
 spec :: Spec
 spec = describe "graceful shutdown" $
@@ -40,12 +40,12 @@ spec = describe "graceful shutdown" $
                 -- block until signaled
                 readMVar allowResponse
                 respond $ responseLBS status200 [("Content-Length", "0")] ""
-            
+
             client sendRequest = do
                 -- first request should return OK
-                sendRequest >>=
-                    (responseStatus <&> (`shouldBe` ok200)) <>
-                    (lookup "Connection" . responseHeaders <&> (`shouldBe` Just "close"))
+                sendRequest
+                    >>= (responseStatus <&> (`shouldBe` ok200))
+                        <> (lookup "Connection" . responseHeaders <&> (`shouldBe` Just "close"))
                 -- wait with the second request
                 void $ readMVar allowSecondRequest
                 -- second request should end with connection refused
@@ -57,28 +57,29 @@ spec = describe "graceful shutdown" $
                 request <- parseRequest ("http://127.0.0.1:" ++ show testPort)
                 withAsync
                     -- start all clients
-                    (replicateConcurrently_ numClients $
-                        client (httpNoBody request manager)) $ \clients -> do
-                    -- wait for all clients to send requests
-                    waitQSemN receivedRequests numClients
-                    -- shutdown the server before serving requests
-                    putMVar shutdownSignal ()
-                    -- wait a little - otherwise some requests might not get
-                    -- Connection: close response header
-                    threadDelay 100_000
-                    -- let requests be handled
-                    putMVar allowResponse ()
-                    -- server should exit
-                    timeout 5_000_000 (wait server) >>=
-                        maybe (expectationFailure "Timeout waiting for server shutdown") pure
-                    -- let clients proceed with the second request
-                    putMVar allowSecondRequest ()
-                    -- wait for all clients and propagate any exceptions
-                    wait clients
-                
-    where
-        -- set number of clients to the number of keep-alive connections
-        numClients = managerConnCount defaultManagerSettings
-        connectionRefused = \case
-            (HttpExceptionRequest _ (ConnectionFailure _)) -> True
-            _ -> False
+                    ( replicateConcurrently_ numClients $
+                        client (httpNoBody request manager)
+                    )
+                    $ \clients -> do
+                        -- wait for all clients to send requests
+                        waitQSemN receivedRequests numClients
+                        -- shutdown the server before serving requests
+                        putMVar shutdownSignal ()
+                        -- wait a little - otherwise some requests might not get
+                        -- Connection: close response header
+                        threadDelay 100_000
+                        -- let requests be handled
+                        putMVar allowResponse ()
+                        -- server should exit
+                        timeout 5_000_000 (wait server)
+                            >>= maybe (expectationFailure "Timeout waiting for server shutdown") pure
+                        -- let clients proceed with the second request
+                        putMVar allowSecondRequest ()
+                        -- wait for all clients and propagate any exceptions
+                        wait clients
+  where
+    -- set number of clients to the number of keep-alive connections
+    numClients = managerConnCount defaultManagerSettings
+    connectionRefused = \case
+        (HttpExceptionRequest _ (ConnectionFailure _)) -> True
+        _ -> False
