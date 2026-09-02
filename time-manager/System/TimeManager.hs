d4d2b7b1f8e4d2ffb9b511d7888cb7eaacd4a93d
Felix Paulusma <felix.paulusma@gmail.com>
2026-09-02 14:18:51 +0200

    time-manager: better comments and don't mask the action in 'withLock'

--- time-manager/System/TimeManager.hs
+++ time-manager/System/TimeManager.hs
@@ -13,7 +13,7 @@
 --   * Using 32-bit systems means the max timeout is @'maxBound' :: Int@
 --     (2147483647) microseconds, which is less than 36 minutes.
 --   * Using the same 'Handle' in different threads might cause issues in some
---     edge cases. (i.e. using cancel/pause in one thread, and resume in another)
+--     edge cases. (i.e. using 'cancel'/'pause' in one thread, and 'resume' in another)
 module System.TimeManager (
     -- ** Types
     Manager,
